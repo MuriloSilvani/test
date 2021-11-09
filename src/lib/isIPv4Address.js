99ebe30629f68cacdbcd249f77c511f5e38731e2
Murilo Silvani <murilo@amo.delivery>
2021-11-09 09:52:09 -0300

    check leading zeros

--- src/lib/isIPv4Address.js
+++ src/lib/isIPv4Address.js
@@ -1,7 +1,7 @@
 const isIPv4Address = (inputString) => {
   const arr = inputString.split('.')
 
-  const checkNumber = num => num && num >= 0 && num <= 255
+  const checkNumber = num => num && `${+num}` === num & num >= 0 && num <= 255
 
   return arr.length === 4 && arr.every(num => checkNumber(num))
 }
